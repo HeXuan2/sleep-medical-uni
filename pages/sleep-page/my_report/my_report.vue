a1b4f572fbaa7f707e18c4ebe1650272a3bd346e
shenlong <2438635312@qq.com>
2024-03-31 17:31:23 +0800

    update

--- pages/sleep-page/my_report/my_report.vue
+++ pages/sleep-page/my_report/my_report.vue
@@ -17,9 +17,11 @@
 	    
 	  </view>
 	  <view class="bt_dash_line"></view>
-	  <view class="card">
+	  <view class="card" v-if="isLoadingSnore">
 	    <view class="load text-grey">鼾声</view>
-	    
+	    <snore_analysis_chart :snoreInfo="snoreInfo"></snore_analysis_chart>
+	    <snore_time_chart :snoreTime="snoreTime"></snore_time_chart>
+	    <evaluate_box :diagnosis="diagnosis_snore"></evaluate_box>
 	  </view>
 	  <view class="bt_dash_line"></view>
 	  <view class="card" v-if="isLoadingPosition">
@@ -42,7 +44,12 @@
 	import position_time_chart from '../../../components/position/position_time_chart/position_time_chart.vue';
 	import evaluate_box from '../../../components/evaluate_box/evaluate_box.vue';
 	import body_move_chart from '../../../components/position/body_move_chart/body_move_chart.vue';
+	
 	import final_diagnosis from '../../../components/final_diagnosis/final_diagnosis.vue';
+	
+	import snore_analysis_chart from '../../../components/snore/snore_analysis_chart/snore_analysis_chart.vue'
+	import snore_time_chart from '../../../components/snore/snore_time_chart/snore_time_chart.vue'
+	
 	import { request } from "/utils/httpUtils.js";
 	import {showToast} from "/utils/ui.js";
 	export default {
@@ -51,7 +58,9 @@
 			position_time_chart,
 			evaluate_box,
 			body_move_chart,
-			final_diagnosis
+			final_diagnosis,
+			snore_analysis_chart,
+			snore_time_chart
 		},
 		data() {
 			return {
@@ -62,11 +71,16 @@
 				positionTime:{},
 				moveExtent:{},
 				diagnosis:{},
+				diagnosis_snore: {},
+				snoreInfo: {},
+				snoreTime: {},
 				isLoadingPosition:false,
-				isLoadingDiagnosis:false
+				isLoadingDiagnosis:false,
+				isLoadingSnore:false,
 			}
 		},
 		methods: {
+			// 获取体位体动信息
 			getPositionData(){
 				let obj = {
 				  method: "GET",
@@ -91,6 +105,7 @@
 			},
 			// 获取医生诊断信息
 			getDiagnosis(){
+			// 获取鼾声信息
 				let obj = {
 				  method: "GET",
 				  showLoading: true,
@@ -106,6 +121,27 @@
 				}).catch(err=>{
 				  showToast("请稍后重试！",1500)
 				});
+			},
+			getSnoreData() {
+				let obj = {
+					method: "GET",
+					showLoading: true,
+					url: `/snore/getData`,
+					data: {
+						reportId: this.reportId
+					},
+					message: "正在获取数据"
+				}
+				request(obj).then(res => {
+					let resData = res.data
+					this.snoreInfo = resData.snoreInfo
+					this.snoreTime = resData.snoreTime
+					this.diagnosis_snore.msg = resData.evaluation
+					this.diagnosis_snore.title = "鼾声"
+					this.isLoadingSnore = true
+				}).catch(err => {
+					showToast("请稍后重试！", 1500)
+				});
 			}
 		},
 		onLoad() {
@@ -113,6 +149,7 @@
 			this.date = this.$route.query.date
 			this.getPositionData()
 			this.getDiagnosis()
+			this.getSnoreData()
 		}
 	}
 </script>
