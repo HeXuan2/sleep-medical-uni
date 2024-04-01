94a8c27f3b26685ab46b236a32c263e847d0b531
shenlong <2438635312@qq.com>
2024-04-01 11:00:46 +0800

    update

--- components/sleep_state/sleep_time/sleep_time.vue
+++ components/sleep_state/sleep_time/sleep_time.vue
@@ -2,7 +2,7 @@
 	<view class="container">
 	  <view class="title">睡眠分期图:</view>
 	  <view class="charts">
-		  <l-echart ref="chart" @finished="initChart"></l-echart>
+		  <l-echart ref="chart"></l-echart>
 	  </view>
 	</view> 
 </template>
@@ -12,43 +12,34 @@
 	export default {
 		data() {
 			return {
-				
+				positionTimeData: [],
+				timeStrings: []
 			}
 		},
+		props: {
+		    stateTime: {
+		      type: Object,
+		      default: () => ({})
+		    }
+		},
 		methods: {
-			generateFixedYValues(timeArray) {
-			  const data = [];
-			  const sleepStages = ['觉醒', '浅睡', '熟睡', 'REM'];
-			  for (let i = 0; i < timeArray.length; i++) {
-			    // 固定在 50 到 70 之间的随机数
-			    const randomIndex = Math.floor(Math.random() * 4);
-			    data.push([timeArray[i], sleepStages[randomIndex]]);
-			  }
-			  return data;
-			},
-			generateTimeArray() {
-			
-			  const startTime = new Date();
-			  startTime.setHours(22, 0, 0, 0);
-			
-			  const endTime = new Date();
-			  endTime.setDate(endTime.getDate() + 1);
-			  endTime.setHours(6, 0, 0, 0);
-			
-			  const timeArray = [];
-			
-			  let currentTime = startTime;
-			  while (currentTime <= endTime) {
-			    const formattedTime = echarts.format.formatTime('hh:mm', currentTime);
-			    timeArray.push(formattedTime);
-			    currentTime = new Date(currentTime.getTime() + 480 * 1000); // 每次增加一分钟
-			  }
-			  return timeArray;
+			dataProcess(){
+				this.stateTime.times = this.stateTime.times.sort((a, b) => a - b);
+				this.timeStrings = this.stateTime.times.map(timestamp => {
+				  const date = new Date(timestamp);
+				  const hours = date.getHours().toString().padStart(2, '0'); // 转换为两位数的小时
+				  const minutes = date.getMinutes().toString().padStart(2, '0'); // 转换为两位数的分钟
+				  return `${hours}:${minutes}`; // 将小时和分钟拼接为字符串
+				});
+				// console.log(this.timeStrings)
+				this.positionTimeData = this.stateTime.sleepStates.map((state, index) => {
+				  return [this.timeStrings[index], state];
+				})
+				// console.log(this.positionTimeData)
 			},
 			init() {
+			  this.dataProcess()
 			  this.$refs.chart.init(echarts, chart => {
-				  const timeArray = this.generateTimeArray();
-				  const randomData = this.generateFixedYValues(timeArray);
 				  const option = {
 				      grid: {
 				        left: '10%',   // 左边距
@@ -58,7 +49,7 @@
 				      },
 				      xAxis: {
 				        type: 'category',
-				        data: timeArray
+				        data: this.timeStrings
 				      },
 				      yAxis: {
 				        type: 'category', // 设置 y 轴类型为分类
@@ -71,7 +62,7 @@
 				        lineStyle: {   // 设置线条的样式
 				          color: '#0099CC' // 修改线条颜色为红色
 				        },
-				        data: randomData // 这里设置你的具体数据，使用二维数组表示 x 和 y 的对应关系
+				        data: this.positionTimeData // 这里设置你的具体数据，使用二维数组表示 x 和 y 的对应关系
 				      }]
 				    }
 			        chart.setOption(option);
